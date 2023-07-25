415172becca9f827b5e561a965821148452d31a7
Saniul haque <haquesaniul@gmail.com>
2023-07-25 13:28:32 +0530

    Persistent favorite list

--- lib/states/notifiers.dart
+++ lib/states/notifiers.dart
@@ -5,7 +5,8 @@ import '../data/models/session_model.dart';
 
 final connectionState = ValueNotifier<ConnectionState>(ConnectionState.none);
 
-final favoriteIPs = ValueNotifier<List<String>>(["5.149.112.247"]);
+final favoriteIPs = PersistentValueNotifier<List<String>>(
+    sharedPreferencesKey: "favorite-ip-list", initialValue: ["5.149.112.247"]);
 
 final selectedIPs =
     ValueNotifier<List<String>>(["5.149.112.247", "153.157.124.113", "74.62.225.21"]);
